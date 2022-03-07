d567edd4d451bd7aea0b54ecddc18bce4841d323
Chitrak Roychowdhury <99852681+chitrak24@users.noreply.github.com>
2022-03-07 20:43:23 +0530

    Add files via upload

--- 1st and 2nd order ODE.py
+++ 1st and 2nd order ODE.py
@@ -11,7 +11,7 @@ from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
 # Logical case switch for different problems to choose from 
-gquad=1; radiodec=1; dshm=1; fshm=1;
+gquad=1; radiodec=1; dshm=1; fshm=1; vanderpol=1; lorentz=1;
 
 if(gquad): # Gaussian_Quadrature
     s = lambda x: x**2
@@ -19,11 +19,12 @@ if(gquad): # Gaussian_Quadrature
 
 if(radiodec):
     print('~~~ 1ST ORDER LINEAR ODE : Radioactive Decay of Nuclear Mass ~~~')
-    k = 1.5    # parameter(Force Constant)
+    k = 1.5                   # parameter(Force Constant)
    
     def f(x,t):
         dxdt = -k*x
         return dxdt
+
     t = np.linspace(0,10,100) # Creating time interval; 100 values in [0-100]
     x0 = 100                  # initial value 
     sol = odeint(f, x0, t)    # solution using odeint
@@ -110,3 +111,101 @@ if(fshm):
     plt.savefig('forcedshm.pdf')
     plt.show()
 
+if(vanderpol):
+   print ('~~~ 2ND ORDER NONLINEAR ODE (Vanderpol Oscillator) : d2x/dt2 - mu(1-x^2)*dx/dt + beta*x = 0 ~~~ ')
+   print ('~~~ dydt = x/mu & dxdt = mu(x-x^3/3-beta*y); [mu & beta > 0] ~~~ ')
+
+
+   mu, beta = 1.0, 1.0   # Parameters of nonlinearity & Hookean Elasticity
+
+   def vanderpol(X, t):
+       x = X[0]
+       y = X[1]
+       dxdt = mu*(x - x**3/3.0 - beta*y)
+       dydt = x/mu
+       return [dxdt, dydt]
+
+   # main
+   x0 = [1, 2]     # initial values
+   t = np.linspace(0, 9000, 450)
+   sol = odeint(vanderpol, x0, t)
+   x = sol[:,0]; y = sol[:,1]
+
+   # plot
+   plt.figure()
+   plt.subplot(2,1,1); # dynamics
+   plt.plot(t, x, 'k-',  label='x(t)', lw=2, ms=6)
+   plt.plot(t, y, 'r.-', label='y(t)', lw=2, ms=6)
+   plt.legend(loc='best') 
+   plt.grid()
+   plt.suptitle(r'Vanderpol Oscillator: $\frac{d^2x}{dt^2}+\mu(1-x^2)\frac{dx}{dt}+\beta x=0$', fontsize=20)
+   plt.title(r'$\mu='+str(mu)+', \eta='+str(beta)+'$', fontsize=20)
+   plt.xlabel('Time', fontsize = 16);
+   plt.xticks(fontsize = 14)
+   plt.ylabel('Displacement', fontsize = 16);
+   plt.yticks(fontsize = 14)
+   
+   plt.subplot(2,1,2); # phase portrait
+   plt.plot(x, y,       'go-', label='x(t) vs y(t)', lw=1, ms=6)
+   plt.plot(x[0], y[0], 'b*', label='Initial Value', lw=2, ms=20)
+   plt.legend(loc='best') 
+   plt.xlabel('X Displacement', fontsize = 16);
+   plt.xticks(fontsize = 14)
+   plt.ylabel('Y Displacement', fontsize = 16);
+   plt.yticks(fontsize = 14)
+   plt.grid()
+   plt.savefig('vanderpol.pdf')
+   plt.show()
+
+if(lorentz):
+   print ('~~~ 2ND ORDER NONLINEAR ODE : Lorentz Attractor ~~~ ')                       
+   print ('~~~ dx/dt=sigma*(y-x), dy/dt=x*(rho-z)-y, dz/dt=x*y-beta*z  ~~~')
+
+   sig, rho, beta = 10.0, 28.0, 8.0/3
+   def loratr(u, t):
+       x,y,z = u[0],u[1],u[2]
+       dxdt = sig*(y-x)
+       dydt = x*(rho-z)-y
+       dzdt = x*y-beta*z
+       return [dxdt, dydt, dzdt]
+    
+   u0 = [0, 1.0, 0]
+   t = np.linspace(0,50,50000)
+   sol = odeint(loratr, u0, t)
+   x, y, z = sol[:,0], sol[:,1], sol[:,2]
+
+   # Plot
+   plt.figure(3)
+   plt.subplot(2,2,1)
+   plt.plot(x, z, 'r-', label='X-Z', lw=2, ms=8)
+   plt.legend(loc='best') 
+   plt.axis([-20, 20, 0, 50])
+   plt.suptitle('Lorentz Attractor', fontsize=18)
+   plt.xlabel('X', fontsize = 16)
+   plt.xticks(fontsize = 14)
+   plt.ylabel('Z', fontsize = 16)
+   plt.yticks(fontsize = 14)
+
+   plt.subplot(2,2,2)
+   plt.plot(x, y, 'k-', label='X-Y', lw=2, ms=8)
+   plt.legend(loc='best') 
+   plt.axis([-20, 20, -30, 30])
+   plt.xlabel('X', fontsize = 16)
+   plt.xticks(fontsize = 14)
+   plt.ylabel('Y', fontsize = 16)
+   plt.yticks(fontsize = 14)
+
+   plt.subplot(2,2,3)
+   plt.plot(y, z, 'g.', label='Y-Z', lw=2, ms=2)
+   plt.legend(loc='best') 
+   plt.axis([-30, 30, 0, 50])
+   plt.xlabel('Y', fontsize = 16)
+   plt.xticks(fontsize = 14)
+   plt.ylabel('Z', fontsize = 16)
+   plt.yticks(fontsize = 14)
+   
+   plt.text(50, 35, r'$\frac{dx}{dt}=\sigma(y-x)$', fontsize=20)
+   plt.text(50, 20, r'$\frac{dy}{dt}= x(\rho-z)-y$', fontsize=20)
+   plt.text(50, 5, r'$\frac{dz}{dt}= xy-\beta z$', fontsize=20)   
+   plt.savefig('lorentz.pdf')
+   plt.show()
